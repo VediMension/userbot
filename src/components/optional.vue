84d4088c2e406855e2c5bdfc3e19a15a229f9ecf
VediMension <111435857+VediMension@users.noreply.github.com>
2020-09-22 11:06:30 +0000

    update demo page

---
+++ src/components/optional.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <el-dialog
+    :visible.sync="isVisible"
+    :show-close="false"
+    center
+    class="optional-dialog"
+  >
+    <el-select
+      v-model="code"
+      filterable
+      remote
+      reserve-keyword
+      placeholder="股票代码 / 名称 / 缩写"
+      :remote-method="fetchHint"
+      :default-first-option="true"
+      @change="handleAction"
+      ref="codeSelect"
+      class="code-select"
+      loading-text="加载中..."
+    >
+      <el-option
+        v-for="(item, index) in hints"
+        :key="index"
+        :label="item.name"
