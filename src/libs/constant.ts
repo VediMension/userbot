d2e6fc14d39089dbcdbdd5cf0ccbadf8eed600f2
VediMension <111435857+VediMension@users.noreply.github.com>
2020-11-27 07:40:23 +0000

    Update from Forestry.io - Updated getting-started-with-ios-apps.md

--- src/libs/constant.ts
+++ src/libs/constant.ts
@@ -2,4 +2,6 @@ const apiUrl = 'http://qt.gtimg.cn/' // 接口地址
 const timeSpan = 3000 // 时间间隔
 const stockIndex = ['1.000001', '0.399001', '0.399006'] // 三大指数
 
-/*
+/**
+ * 比较两个金额大小并返回相应类名
+ * @param value 被
