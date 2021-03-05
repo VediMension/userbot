45171c672ce92fda57cf913a835e924b79c69efa
VediMension <111435857+VediMension@users.noreply.github.com>
2021-03-05 14:02:16 +0000

    Fix README markup

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,5 @@ import Vue from 'vue'
 import axios from 'axios'
 import Toasted from 'vue-toasted' // 底部提示弹窗
 import {Table, TableColumn, Input, Select, Option, Form, FormItem, Dialog, Button, Loading} from 'element-ui' // 按需加载
-import 'element-ui/lib/theme-chalk/index.
+import 'element-ui/lib/theme-chalk/index.css'
+import App from './App.vue'
