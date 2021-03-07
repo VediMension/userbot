228f1585c1b8faa62c3340054ee9c18d8a85ebd3
VediMension <111435857+VediMension@users.noreply.github.com>
2021-03-07 11:07:35 +0000

    updates internal dependencies

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,5 @@ import Toasted from 'vue-toasted' // 底部提示弹窗
 import {Table, TableColumn, Input, Select, Option, Form, FormItem, Dialog, Button, Loading} from 'element-ui' // 按需加载
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
+import router from './router'
+import 
