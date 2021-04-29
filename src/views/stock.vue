6552e15ab2c4171416cde0ea14c1d1ba97817e22
VediMension <111435857+VediMension@users.noreply.github.com>
2021-04-29 17:27:31 +0000

    Fix merge issues

--- src/views/stock.vue
+++ src/views/stock.vue
@@ -48,3 +48,31 @@
         >
           <span>{{ stock.limit.up.toFixed(2) }}</span>
         </el-form-item>
+        <el-form-item label="成交量:">
+          <span>{{ transVolume(stock.volume.total) }}手</span>
+        </el-form-item>
+        <el-form-item
+          label="最低:"
+          :class="comparePrice(stock.lowest, stock.yesterday)"
+        >
+          <span>{{ stock.lowest.toFixed(2) }}</span>
+        </el-form-item>
+        <el-form-item label="昨收:">
+          <span>{{ stock.yesterday.toFixed(2) }}</span>
+        </el-form-item>
+        <el-form-item
+          label="跌停:"
+          :class="comparePrice(stock.limit.down, stock.yesterday)"
+        >
+          <span>{{ stock.limit.down.toFixed(2) }}</span>
+        </el-form-item>
+        <el-form-item label="成交额:">
+          <span>{{ stock.volume.turn }}万</span>
+        </el-form-item>
+        <el-form-item label="换手:">
+          <span>{{ stock.turnover.toFixed(2) }}%</span>
+        </el-form-item>
+        <el-form-item label="总市值:">
+          <span>{{ stock.cap }}亿</span>
+        </el-form-item>
+        <el-form-item label="市净率:">
